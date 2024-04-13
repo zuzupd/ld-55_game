50570e27195e4a8c0cd7f893cda3c7a02125d174
Ondrej Dovec <ondrejdovec@gmail.com>
2024-04-13 12:18:47 +0200

    Add player

--- Menu/main_menu.gd
+++ Menu/main_menu.gd
@@ -5,17 +5,14 @@ func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 
 
-func _on_new_fps_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/fps_level.tscn")
-
-
-func _on_new_isometric_button_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_new_3_rd_person_button_pressed() -> void:
-	pass # Replace with function body.
-
-
 func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+
+func _on_new_game_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_about_button_pressed() -> void:
 	pass # Replace with function body.
